c1cd994a6a4cc5af19c39df278929711dba0f7d0
Mithilesh Pradhan <32608901+mithilesh1coder@users.noreply.github.com>
2020-08-16 13:40:54 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { MultiStepFormComponent } from './multi-step-form/multi-step-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MultiStepFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
